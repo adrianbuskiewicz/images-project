e1e1ba8b54ecd25a443812f214cca4e936c38fd6
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2022-06-13 21:46:36 +0200

    New urls for expiring links and expiring images.

--- images/urls.py
+++ images/urls.py
@@ -2,11 +2,16 @@ from django.urls import path, include
 from .views import (
     ImageUploadView,
     ImageListView,
+    ExpiringLinkCreateView,
+    expiring_image_view,
 )
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls')),
     path('', ImageListView.as_view(), name='image_list'),
     path('upload/', ImageUploadView.as_view(), name='image_upload'),
+    path('expiring/', ExpiringLinkCreateView.as_view(), name='image_expiring_link'),
+    path('<token>/', expiring_image_view, name='expiring_image')
 ]
+
 
