16a2243369155a5f6144c19ca9e21303fb92ed88
Adrian Buśkiewicz <adrian.k.buskiewicz@gmail.com>
2022-06-13 21:46:46 +0200

    Created expiring link logic in views.

--- images/views.py
+++ images/views.py
@@ -1,9 +1,17 @@
-from rest_framework import generics, views, status
+from rest_framework import generics, views, status, viewsets
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import permissions
 from .models import ImageFile
-from .serializers import ImageSerializer
+from .serializers import ImageSerializer, ExpiringLinkCreateSerializer
+from .permissions import LinksCreatingPermission
+from .models import ExpiringLink
+from django.utils import timezone
+import datetime
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from secrets import token_urlsafe
 
 # Create your views here.
 
@@ -22,6 +30,11 @@ class ImageUploadView(views.APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class ImageDetailView(generics.RetrieveAPIView):
+    queryset = ImageFile.objects.all()
+    serializer_class = ImageSerializer
+
+
 class ImageListView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ImageSerializer
@@ -35,6 +48,37 @@ class ImageListView(generics.ListCreateAPIView):
             return None
 
 
+class ExpiringLinkCreateView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated, LinksCreatingPermission]
+    parser_classes = [MultiPartParser, FormParser]
+
+    def post(self, request, format=None):
+        serializer = ExpiringLinkCreateSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            data = serializer.data
+            img = get_object_or_404(ImageFile, id=data['img_id'])
+            token = token_urlsafe(20)
+            url = request.build_absolute_uri(f'/api/{token}')
+            print(url)
+            link = ExpiringLink(
+                image=img,
+                token=token,
+                expiring_url=url,
+                time_to_expire=timezone.now()+datetime.timedelta(seconds=data['seconds']),
+            )
+            link.save()
+            return Response(url, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
+@api_view(['GET'])
+def expiring_image_view(request, token):
+    try:
+        token = ExpiringLink.objects.get(token=token, time_to_expire__gte=timezone.now())
+        image = get_object_or_404(ImageFile, id=token.image.id).original_image
+        extension = str(image).split('.')[1]
+        extension = 'jpeg' if extension in ('jpg', 'jpeg') else 'png'
+        return HttpResponse(image, content_type=f"image/{extension}")
+    except ExpiringLink.DoesNotExist:
+        return Response('Link\'s time expired!', status=status.HTTP_400_BAD_REQUEST)
